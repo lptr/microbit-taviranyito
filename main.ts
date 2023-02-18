405d4ec73e6c9aa014cd4a3d72ffec7a1693e0fe
Lóránt Pintér <lorant.pinter@gmail.com>
2023-02-18 17:39:09 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+radio.setGroup(1)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        radio.sendString("menj")
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        radio.sendString("balra")
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        radio.sendString("jobbra")
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
